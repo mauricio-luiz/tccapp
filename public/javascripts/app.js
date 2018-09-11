56dec047dfd160a9a980ebdea24bfdac69ccc5f0
mauricio <mauriciojunio@gmail.com>
2018-09-11 20:42:59 -0300

    adiciona layout de salas definindo tambem listagem geral

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -13,4 +13,11 @@ if( $('#botao-adicionaOpcao') !== null){
     .addEventListener('click', opcao.adiciona.bind(opcao));
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+        coverTrigger : false,
+        constrainWidth : false
+    });
+});
 
