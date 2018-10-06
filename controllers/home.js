fa1be8dd9720dfde4ff277dda4370c15d9d96555
mauricio <mauriciojunio@gmail.com>
2018-10-06 20:52:48 -0300

    refatora login, registro de professor e aluno

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 const md5 = require('md5');
-
+const querystring = require('querystring');
 module.exports = (app) => {
     const Usuario = app.models.usuario;
     const Professor = app.models.professor;
@@ -42,17 +42,29 @@ module.exports = (app) => {
             res.redirect('/');
         },
         professor(req, res){
-            res.render('home/professor');
+            res.render('home/professor', {message : req.query });
         },
         aluno(req, res){
-            res.render('home/aluno');
+            res.render('home/aluno',  {message : req.query });
         },
         registrarProfessor(req, res){
             const { usuario } = req.body;
-            const {nome, email}  = usuario;
+            const {nome, email, password_confirmation}  = usuario;
             const password = md5(usuario.password);
             const where = { email, password};
             const tipo = 'p';
+
+            if(md5(password_confirmation) != password){
+                const message = querystring.stringify({
+                        password : 'Senhas não conicidem. Tente novamente!',
+                        nome : nome,
+                        email : email
+                    }
+                );
+                res.redirect(`/registrar/professor?${message}`);
+                return;
+            }
+
             const set = {
                 $setOnInsert: { nome : nome, email, password, tipo }
             };
@@ -70,17 +82,34 @@ module.exports = (app) => {
                         res.redirect('/professor');
                     });
                 })
-                .catch( (e) => { 
-                    console.log(e);
-                    res.redirect('/registrar/professor', {message : e.message});
+                .catch( (e) => {
+                    const message = querystring.stringify({
+                            usuario : 'Usuário já em uso',
+                            nome : nome,
+                            email : email
+                        }
+                    );
+                    res.redirect(`/registrar/professor?${message}`);
                 });
         },
         registrarAluno(req, res){
             const { usuario } = req.body;
-            const {nome, email}  = usuario;
+            const {nome, email, password_confirmation}  = usuario;
             const password = md5(usuario.password);
             const where = { email, password};
             const tipo = 'a';
+
+            if(md5(password_confirmation) != password){
+                const message = querystring.stringify({
+                        password : 'Senhas não conicidem. Tente novamente!',
+                        nome : nome,
+                        email : email
+                    }
+                );
+                res.redirect(`/registrar/aluno?${message}`);
+                return;
+            }
+
             const set = {
                 $setOnInsert: { nome : nome, email, password, tipo }
             };
@@ -98,8 +127,13 @@ module.exports = (app) => {
                     });
                 })
                 .catch( (e) => { 
-                    console.log(e);
-                    res.redirect('/registrar/aluno', {message : e.message});
+                    const message = querystring.stringify({
+                            usuario : 'Usuário já em uso',
+                            nome : nome,
+                            email : email
+                        }
+                    );
+                    res.redirect(`/registrar/aluno?${message}`);
                 });
         }
     }
