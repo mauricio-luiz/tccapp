1d3ef038661c3c9c313bdb2a8c92b1b36d22f4dc
mauricio <mauriciojunio@gmail.com>
2018-10-06 21:39:30 -0300

    adiciona apresentaçao para os usuarios apos login

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -1,8 +1,8 @@
 module.exports = (app) => {
     const AlunoController = {
         index(req, res){
-            const usuario = req.session;
-            res.render('aluno/index');
+            const { usuario } = req.session;
+            res.render('aluno/index', { usuario });
         }
     };
     return AlunoController;
